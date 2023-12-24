9ca30545ed7751bf6dd80881d4742641fd182926
Ben Perram <ben.perram@lumi.com.au>
2023-12-24 18:45:09 +1100

    changed event details that where being set in calenda and centered things

--- js/scripts.js
+++ js/scripts.js
@@ -157,23 +157,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Ben and Chloe's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Feb 11, 2024 10:30'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Feb 11, 2024 15:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Level 24/227 Elizabeth St, Sydney NSW 2000',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact Chloe at 0405403364 or Ben at 0424217173."
         }
     });
 
